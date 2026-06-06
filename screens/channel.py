45c18421332223f71970e6cfccb3b8be44302c0a
Gabby Morgan <gabriellarosemorgan@proton.me>
2026-06-06 00:14:11 -0500

    selecting channel calls relay

--- screens/channel.py
+++ screens/channel.py
@@ -1,5 +1,7 @@
 from enum import StrEnum, auto
 
+import RNS
+
 class ChannelState(StrEnum):
     READY = auto()
     CONNECTING = auto()
@@ -11,17 +13,32 @@ class ChannelScreen:
     def __init__(self, app):
         self.app = app
         self.phone_state = ChannelState.READY
+        self.channel_hash = None
+        self.channel_name = None
         
     def mount(self):
-        self.app.ui.on_press(self._on_press)
-        self.app.ui.on_release(self._on_release)
-        self.app.ui.on_long_press(self._on_long_press)
-        self.app.ui.on_long_release(self._on_long_release)
-        self.app.ui.on_double_press(self._on_double_press)
-        self.app.ui.on_double_release(self._on_double_release)
-        self.app.state.set_on_change_callback(self._on_app_change)
-        
-        self.display()
+        try:
+            self.app.ui.on_press(self._on_press)
+            self.app.ui.on_release(self._on_release)
+            self.app.ui.on_long_press(self._on_long_press)
+            self.app.ui.on_long_release(self._on_long_release)
+            self.app.ui.on_double_press(self._on_double_press)
+            self.app.ui.on_double_release(self._on_double_release)
+            self.app.state.set_on_change_callback(self._on_app_change)
+            
+            relay_name, relay_hash = self.app.state.current_channel
+            print(bytes.fromhex(relay_hash))
+            relay_identity = RNS.Identity.recall(bytes.fromhex(relay_hash))
+            if relay_identity is None:
+                print("Could not discover channel identity")
+                self.app.router.navigate("channels_list")
+            self.app.telephone.call(relay_identity)
+            self.channel_hash = relay_hash
+            self.channel_name = relay_name
+            
+            self.display()
+        except Exception as e:
+            print(e)
         
     def _on_press(self):
         pass
